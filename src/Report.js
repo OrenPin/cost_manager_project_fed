67d301637dd52c1ac8a33c1c492781101817e7be
OrenPin <orenpin97@gmail.com>
2023-02-04 14:11:03 +0200

    making a chart

--- src/Report.js
+++ src/Report.js
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { getData } from './localStorage';
+import ReportPieChart from './Chart';
 
 const Report = (props) => {
+
+    const fakeData = [
+        { Category: 'Food', Sum: 100 },
+        { Category: 'Transport', Sum: 200 },
+        { Category: 'Clothes', Sum: 300 },
+        { Category: 'Entertainment', Sum: 400 }
+    ];
 
     return (
         <div className= "reportDiv" style={{display: props.visible ? 'flex' : 'none'}}>
@@ -16,17 +22,31 @@ const Report = (props) => {
                     {props.displayedCosts?.map((cost, index) => {
                         return (
                             <tr className='tableRow' key={index}>
-                                <td>{cost.Date}</td>
                                 <td>{cost.Category}</td>
                                 <td>{cost.Quantity}</td>
                                 <td>{cost.Description}</td>
                                 <td>{cost.Sum}</td>
+                                <td>{cost.Date}</td>
                             </tr>
                         );
                     })}
                 </tbody>
                 </table>
                 <div className='graphDiv'>
+                    let categoryCount = {
+                        props.displayedCosts.forEach((cost) => {
+                            if (categoryCount[cost.Category]) {
+                                categoryCount[cost.Category]++;
+                            } else {
+                                categoryCount[cost.Category] = 1;
+                            }
+                        })}
+                        let categoryCountArray = [];
+                        for (let category in categoryCount) {
+                            categoryCountArray.push({Category: category, Sum: categoryCount[category]});
+                        };
+
+                    <ReportPieChart data={categoryCountArray} />
                 </div>
             </div> 
             </div>  
