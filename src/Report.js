02eeaf0a607caffd63c8f3e8cf0ce200633cf7cc
OrenPin <orenpin97@gmail.com>
2023-02-04 15:27:28 +0200

    chart

--- src/Report.js
+++ src/Report.js
@@ -1,18 +1,39 @@
 import ReportPieChart from './Chart';
+import { useEffect, useState } from 'react';
 
 const Report = (props) => {
 
+    const f = (displayedCosts) => {
+        let categoryCount ={};
+        displayedCosts?.forEach((cost) => {
+            if (cost.Category in categoryCount)
+                categoryCount[cost.Category] += Number(cost.Sum);
+            else
+                categoryCount[cost.Category] = Number(cost.Sum);
+        });
+        let x = []
+        Object.keys(categoryCount)?.forEach((category) => {
+            if (category == 'undefined')
+                category = 'Other';
+             x.push({Category: category, Sum: categoryCount[category]});
+        });
+        return x;
+    }
+
+    const [data, setData] = useState(); // data for the pie chart (category and sum)
     const fakeData = [
         { Category: 'Food', Sum: 100 },
         { Category: 'Transport', Sum: 200 },
         { Category: 'Clothes', Sum: 300 },
         { Category: 'Entertainment', Sum: 400 }
     ];
+         
+    useEffect(() => {setData(f(props.displayedCosts));}, [props.displayedCosts]);
 
     return (
-        <div className= "reportDiv" style={{display: props.visible ? 'flex' : 'none'}}>
+        <div className= "reportDiv">
             <div className='graphAndTableContainer'>         
-                <table className = "costsTable" >
+                <table className = "Table" >
                 <thead >
                     <tr className='tableHeader'>
                     {props.fields.map((field, index) => {return <th key={index}>{field.label}</th>})}
@@ -33,20 +54,7 @@ const Report = (props) => {
                 </tbody>
                 </table>
                 <div className='graphDiv'>
-                    let categoryCount = {
-                        props.displayedCosts.forEach((cost) => {
-                            if (categoryCount[cost.Category]) {
-                                categoryCount[cost.Category]++;
-                            } else {
-                                categoryCount[cost.Category] = 1;
-                            }
-                        })}
-                        let categoryCountArray = [];
-                        for (let category in categoryCount) {
-                            categoryCountArray.push({Category: category, Sum: categoryCount[category]});
-                        };
-
-                    <ReportPieChart data={categoryCountArray} />
+                    <ReportPieChart data={data} />
                 </div>
             </div>
         </div>
