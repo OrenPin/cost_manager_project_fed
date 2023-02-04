26ff27eddfa596b048c478daf9e8f5d128971533
eylamk <eylamkadden@gmail.com>
2023-02-04 20:11:33 +0200

    fixed grpah

--- src/Chart.js
+++ src/Chart.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { PieChart, Pie, Cell, Tooltip} from "recharts";
+import { PieChart, Pie, Cell, Tooltip } from "recharts";
 
 const ReportPieChart = ({ data }) => {
-    // const COLORS = [ 'red', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'grey', 'black'];
+    const COLORS = ['red', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'grey', 'black'];
     return (
         <PieChart width={400} height={400} className="pieChart">
-            <Pie 
+            <Pie
                 data={data}
-                dataKey="Sum" 
-                nameKey="Category" 
-                cx= {200}
-                cy= {200} 
-                fill="#000"
+                dataKey="Sum"
+                nameKey="Category"
+                cx={200}
+                cy={200}
+                fill={null}
                 innerRadius={90}
-                outerRadius= {120}
+                outerRadius={120}
                 paddingAngle={2}
                 label
-                >
-                     {/* {data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
-                 } */}
+            >
+                {data?.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)
+                }
             </Pie>
-                <Tooltip />
+            <Tooltip />
         </PieChart>
     );
 };
