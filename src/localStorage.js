50e547c8fdd29cc24c48996b50749df07f8ee5eb
eylamk <eylamkadden@gmail.com>
2023-02-06 18:41:31 +0200

    finished next nexet

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,13 +1,13 @@
 // Save data to local storage using async method
 export const saveData = async (key, value) => {
-    let newCosts= await getData(key);
-    newCosts = newCosts.concat(value)
-    localStorage.setItem(key,JSON.stringify(newCosts));
-  };
-  
+  let newCosts = await getData(key);
+  newCosts = newCosts.concat(value)
+  localStorage.setItem(key, JSON.stringify(newCosts));
+};
+
 // Get data from local storage using async method
 export const getData = async (key) => {
-    let data = localStorage.getItem(key);
-    return JSON.parse(data) || [];
-  };
+  let data = localStorage.getItem(key);
+  return JSON.parse(data) || [];
+};
 
